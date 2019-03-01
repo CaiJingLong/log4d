78004db6a66d64d4f97478c6e528968cfbc3be97
Caijinglong <cjl_spy@163.com>
2019-03-01 10:32:35 +0800

    update version 1.0.2

--- lib/src/log/timestamp.dart
+++ lib/src/log/timestamp.dart
@@ -1,3 +1,4 @@
 part of '../log.dart';
 
-now({ color: gray }) => '[${color(new DateTime.now().toString().substring(11, 19))}]';
+now({color: gray}) =>
+    '[${color(new DateTime.now().toString().substring(11, 19))}]';
